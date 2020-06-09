86d03237114c71942f8e929d99d370347ba3e64a
Pedro Revilla <peterevilla@gmail.com>
2020-06-08 21:47:57 -0500

    wip

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/users.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    pool:{
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+  },
+  },
+
+};
